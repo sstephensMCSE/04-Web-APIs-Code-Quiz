ab597d408bbdc8adb883603d33a56b702c81b801
sstephensMCSE <sstephens.mcse@gmail.com>
2020-10-08 18:07:10 -0400

    created the intro screen

--- script.js
+++ script.js
@@ -36,20 +36,31 @@ var a_text = document.getElementById('a_text');
 var b_text = document.getElementById('b_text');
 var c_text = document.getElementById('c_text');
 var d_text = document.getElementById('d_text');
-
 var submitbtn = document.getElementById('submit')
+var list = document.getElementById('list')
+var paragraph = document.getElementById('paragraph')
+
+// hide the quiz list
+list.style.display = "none";
+questionEl.innerText = "Welcome to the quiz!";
+submitbtn.innerText = "Proceed";
+paragraph.innerText = "This is the instructions";
+
+
 
 // initialize the counter for the curent question
-currentQuestion = 0;
+currentQuestion = -1;
 
 // this function will call the quiz...
 //ToDo: remove the initial quiz and replace it with a welcome screen.
 
-loadQuiz();
+
+//loadQuiz();
 
 
 // use JS to load the quiz data into the HTML
 function loadQuiz() {    
+    
     // Load the curent quiz into new variable
     var currentQuizData = quizData[currentQuestion];
     // set the question and answer texts
@@ -61,27 +72,34 @@ function loadQuiz() {
 }
 
 submitbtn.addEventListener('click', function() {
-    // get the radio values
-    var answer = detectAnswer();
-    // todo: warn user if there is no answer selected
-    if (answer == "na") {
-        alert("Please select one answer")
-    } else {        
+    if (currentQuestion == -1) {
+        submitbtn.innerText = "Submit";
+        list.style.display = "";
+        paragraph.innerText = "";
         currentQuestion++
-        document.getElementById(answer).checked = false;
-        if (currentQuestion < quizData.length) {
-            loadQuiz();
-        } else {
-            // todo: all done
-            // hide the list
-            // your final score was score
-            // enter initials
-            // submit
-            // show leaderboard
-            alert("end")
+        loadQuiz();
+    } else {
+        // get the radio values
+        var answer = detectAnswer();
+        // todo: warn user if there is no answer selected
+        if (answer == "na") {
+            alert("Please select one answer")
+        } else {        
+            currentQuestion++
+            document.getElementById(answer).checked = false;
+            if (currentQuestion < quizData.length) {
+                loadQuiz();
+            } else {
+                // todo: all done
+                // hide the list
+                // your final score was score
+                // enter initials
+                // submit
+                // show leaderboard
+                alert("end")
+            }
         }
     }
-    
 })
 
 function detectAnswer(){
