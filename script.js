ebc2fa9c6528456a7de2ad10f9234776901c3c14
sstephensMCSE <sstephens.mcse@gmail.com>
2020-10-08 16:55:20 -0400

    created logic for the quiz

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initialize quiz data as constant since it will be global unchanging
-const quizObj = [
+const quizData = [
     {
         question: 'question 1?',
         answer: 'a',
@@ -31,20 +31,63 @@ const quizObj = [
     }
 ];
 
+var questionEl = document.getElementById('question');
+var a_text = document.getElementById('a_text');
+var b_text = document.getElementById('b_text');
+var c_text = document.getElementById('c_text');
+var d_text = document.getElementById('d_text');
 
+var submitbtn = document.getElementById('submit')
 
-
-
-// counter for the curent question
+// initialize the counter for the curent question
 currentQuestion = 0;
 
-// this function will call the quiz
+// this function will call the quiz...
+//ToDo: remove the initial quiz and replace it with a welcome screen.
+
 loadQuiz();
 
-function loadQuiz() {
-
-
-}
 
 // use JS to load the quiz data into the HTML
+function loadQuiz() {    
+    // Load the curent quiz into new variable
+    var currentQuizData = quizData[currentQuestion];
+    // set the question and answer texts
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;   
+    d_text.innerText = currentQuizData.d;
+}
 
+submitbtn.addEventListener('click', function() {
+    // get the radio values
+    var answer = detectAnswer();
+    // todo: warn user if there is no answer selected
+    if (answer == "na") {
+        alert("Please select one answer")
+    } else {        
+        currentQuestion++
+        document.getElementById(answer).checked = false;
+        if (currentQuestion < quizData.length) {
+            loadQuiz();
+        } else {
+            // todo: all done
+            // hide the list
+            // your final score was score
+            // enter initials
+            // submit
+            // show leaderboard
+            alert("end")
+        }
+    }
+    
+})
+
+function detectAnswer(){
+    if (document.getElementById('a').checked) {return "a"}
+    else if (document.getElementById('b').checked) {return "b"}
+    else if (document.getElementById('c').checked) {return "c"}
+    else if (document.getElementById('d').checked) {return "d"}
+    else return "na"
+};
