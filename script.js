29be494fa34467cfd6c7ae347ed2f95875506511
sstephensMCSE <sstephens.mcse@gmail.com>
2020-10-08 16:02:58 -0400

    styled the question box

---
+++ script.js
@@ -0,0 +1,50 @@
+// Initialize quiz data as constant since it will be global unchanging
+const quizObj = [
+    {
+        question: 'question 1?',
+        answer: 'a',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',        
+    },    {
+        question: 'question 2?',
+        answer: 'b',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',        
+    },    {
+        question: 'question 3?',
+        answer: 'c',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',        
+    },    {
+        question: 'question 4?',
+        answer: 'd',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',        
+    }
+];
+
+
+
+
+
+// counter for the curent question
+currentQuestion = 0;
+
+// this function will call the quiz
+loadQuiz();
+
+function loadQuiz() {
+
+
+}
+
+// use JS to load the quiz data into the HTML
+
